b0633508abd9256309ab7cf3c7442f6ec811423b
Garfdev <garfdev.13@gmail.com>
2023-02-08 16:21:22 +0700

    feat: init project

--- src/bootstap.ts
+++ src/bootstap.ts
@@ -1,8 +1,8 @@
-import LoadController from "./controllers/loader.controller";
-import CurrencyController from "./controllers/currency.controller";
 import chalk from "chalk";
-import { IResponse } from "./types";
 import sequelize from "./configs/sequelize";
+import CurrencyController from "./controllers/currency.controller";
+import LoadController from "./controllers/loader.controller";
+import { IResponse } from "./types";
 
 const bootstrap = async () => {
   let response: IResponse | null = null;
@@ -14,24 +14,29 @@ const bootstrap = async () => {
 
   if (!args[0]) {
     response = await CurrencyController.statictic();
-    return console.log(chalk[response.type](response.message));
-  }
-
-  switch (args[0]) {
-    case "load": {
-      response = await LoadController.load(args[1]);
-      break;
-    }
-    default: {
-      const isDate = Date.parse(args[0]);
-      if (isDate) {
-        response = await CurrencyController.statictic(args[0]);
-
-      } else {
-        response = await CurrencyController.read(args[0]);
+  } else {
+    switch (args[0]) {
+      case "load": {
+        response = await LoadController.load(args[1]);
+        break;
+      }
+      default: {
+        const isDate = Date.parse(args[0]);
+        if (isDate) {
+          response = await CurrencyController.statictic(args[0]);
+        } else {
+          response = await CurrencyController.read(args[0]);
+        }
       }
     }
   }
+
+  if (response.message === "{}")
+    return console.log(
+      chalk.red(
+        "There not yet any portfolio data, please load your CSV file using `portfolio-cli load :path_to_csv_file:` command"
+      )
+    );
 
   if (!response) return console.log(chalk.red("Invalid Paramaters"));
 
